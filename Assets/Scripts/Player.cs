44dce568f70f6166b6392fb7060bbc7df74d0997
Luramoth <85266594+Luramoth@users.noreply.github.com>
2023-02-12 23:08:09 -0800

    do it again

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -209,7 +209,7 @@ public class Player : MonoBehaviour
 
 		if(direction.magnitude > 0.1f)
 		{
-			float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+			float targetAngle = (Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg) + cam.eulerAngles.y;
 
 			Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.right;
 
